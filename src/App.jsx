c0e732f502457fc08098f0ff0ec2eac1873d3d76
Jinwoo Han <56533546+jin00woo@users.noreply.github.com>
2022-11-24 11:59:26 +0000

    Used useeffect hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,61 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, Fragment } from "react";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  
+  useEffect(() => {
+    if(isInitial){
+      isInitial = false;
+      return;
+    }
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data"
+      }));
+      
+      const response = await fetch("https://food-order-app-42cba-default-rtdb.firebaseio.com/cart.json", {method: "PUT", body: JSON.stringify(cart)});
+    };
+
+    if(!response.ok){
+      dispatch(uiActions.showNotification({
+      status: "error",
+      title: "Error!",
+      message: "Sending data failed."
+    }));
+    }
+
+    dispatch(uiActions.showNotification({
+      status: "success",
+      title: "Success!",
+      message: "Sent data successfully"
+    }));
+    
+  }, [cart]);
+  
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message}></Notification>}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+    
   );
 }
 
